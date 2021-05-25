5eda1d9ea5aac8bbd09a94d88466ef8f5a860313
Lori <lorikxi@icloud.com>
2021-05-25 14:57:59 +0200

    Added lottie on 404 (not finished)

--- src/pages/FileNotFound/index.js
+++ src/pages/FileNotFound/index.js
@@ -1,7 +1,17 @@
 import React from "react";
+import { Header, Footer } from "../../components";
+import Lottie from "../../components/404_Lottie";
 
 const index = () => {
-  return <div>file not found</div>;
+  return (
+    <div>
+      <Header />
+      <div className="relative bg-purple-500 flex justify-center">
+        <Lottie x={"80%"} y={"auto"} />
+      </div>
+      <Footer />
+    </div>
+  );
 };
 
 export default index;
